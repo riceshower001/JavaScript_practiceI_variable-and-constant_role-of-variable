b58ac54f3f43a37c9781cc9519ae504365dfdf8b
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 12:05:29 +0900

    Add files via upload

---
+++ JavaScript_practiceI_variable and constant_role of variable.js
@@ -0,0 +1,8 @@
+// Please define the variable "length".
+let length = 5 ;
+
+// Please output the value of the variable "length".
+console.log(length) ;
+
+// Please use the variable "length to output the circle area.
+console.log(length * length * 3) ;
